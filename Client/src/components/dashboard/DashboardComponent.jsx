d03628c3879049e208c18e2d37677f15d97c91ba
bartoszstepak <bartoszstepak@gmail.com>
2022-10-18 01:02:13 +0200

    added routing

---
+++ Client/src/components/dashboard/DashboardComponent.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class DashboardComponent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {  };
+      }
+
+      render() {
+        return (
+            <div>
+              <h2>Dashboard</h2>
+            </div>
+        );
+      }
+    
+}
+
+export default DashboardComponent;
