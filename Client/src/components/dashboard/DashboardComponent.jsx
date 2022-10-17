be0cbb6e7d12a21681d7866dd6beb77446b107f5
bartoszstepak <bartoszstepak@gmail.com>
2022-10-18 01:03:58 +0200

    dashbaord init

--- Client/src/components/dashboard/DashboardComponent.jsx
+++ Client/src/components/dashboard/DashboardComponent.jsx
@@ -2,19 +2,21 @@ import React, { Component } from 'react';
 
 class DashboardComponent extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {  };
-      }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-      render() {
-        return (
-            <div>
-              <h2>Dashboard</h2>
-            </div>
-        );
-      }
-    
+  render() {
+    return (
+      <div>
+        
+        <h2>Dashboard</h2>
+
+      </div>
+    );
+  }
+
 }
 
 export default DashboardComponent;
