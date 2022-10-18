475e34950cfe317aa0a4ee8ed31e1c58dde5093b
bartoszstepak <bartoszstepak@gmail.com>
2022-10-18 02:07:20 +0200

    ts config, trainer MODEL

---
+++ Client/src/components/dashboard/DashboardComponent.tsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+import './DashboardComponent.css'
+
+class DashboardComponent extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+  }
+
+  componentWillUnmount() {
+  }
+
+  render() {  
+    return (
+      <div>
+        
+        <h2>Dashboard</h2>
+
+      </div>
+    );
+  }
+
+}
+
+export default DashboardComponent;
